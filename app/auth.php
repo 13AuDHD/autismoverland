78cb4d1326ffdfff6f00f1e3b090c88e9da6dfbe
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-23 04:09:15 -0600

    Add memberships.php to auth.php dependencies

--- app/auth.php
+++ app/auth.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/scout-maintenance.php';
+require_once __DIR__ . '/memberships.php';
 
 
 /* =========================================================
