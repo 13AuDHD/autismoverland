ae62ed0e8a9a7c5a1a61392f963e639a54c1fe8a
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-29 19:04:29 -0600

    Include accessibility.js script in MFA setup
    Added accessibility script to mfa-setup.php

--- account/mfa-setup.php
+++ account/mfa-setup.php
@@ -704,6 +704,9 @@ $displayName =
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
   >
 
+   <script
+  src="https://llamascout.com/js/accessibility.js"
+></script>
 
   <?php if (
       !$enabled
