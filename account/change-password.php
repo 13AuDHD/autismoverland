bc4712f9d8595aeb2b7d05febbcf14fd516d892f
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-30 10:17:38 -0600

    Remove accessibility.js script from change-password.php
    Removed accessibility.js script from change-password.php

--- account/change-password.php
+++ account/change-password.php
@@ -130,9 +130,6 @@ $displayName = trim(
   <link rel="stylesheet" href="https://llamascout.com/css/style.css">
   <link rel="stylesheet" href="https://llamascout.com/css/account.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-<script
-  src="https://llamascout.com/js/accessibility.js"
-></script>
 </head>
 
 <body class="account-body">
